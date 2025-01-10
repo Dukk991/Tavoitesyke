c688ae271fb5979eef0f603e319dc914f32add2b
Valtteri <valtteri.putaansuu@gmail.com>
2025-01-10 12:04:20 +0200

    tehtävä tehty

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [age, setAge] = useState('0')
+  const [limit, setLimit] = useState('')
+
+  const ageLimitSet = () => {
+
+    if (age <= 0 || age > 100) {
+      setLimit("Invalid input")
+    } else if (age > 0) {
+      const lower = (220 - age) * 0.65
+      const upper = (220 - age) * 0.85
+      const result = lower.toFixed(0) + '-' + upper.toFixed(0)
+      setLimit(result)
+    } else {
+      setLimit("Invalid input")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        style={styles.field}
+        value={age}
+        onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'
+      ></TextInput>
+      <Text style={styles.field}>Limit</Text>
+      <Text style={styles.field}>{limit}</Text>
+      <Button title="Calculate" onPress={ageLimitSet}></Button>
     </View>
   );
 }
@@ -17,4 +43,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  field: {
+    marginBottom: 10,
+  }
 });
